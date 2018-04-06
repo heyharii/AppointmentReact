0a1be8dccadb8f61c029069e7485f10533fed77e
hari <neogalaxy45@gmail.com>
2018-04-06 23:24:57 +0700

    Remove react-rails gem. Add react_on_rails gem.

---
+++ app/javascript/packs/appointment_form.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Datetime from 'react-datetime'
+import 'react-datetime/css/react-datetime'
+
+export default class AppointmentForm extends React.Component {
+    
+    handleChange(e) {
+        const name = e.target.name;
+        obj = {};
+        obj[name] = e.target.value;
+        this.props.onUserInput(obj);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.onFormSubmit();
+    }
+
+    setApptTime(e){
+        const name = 'appt_time';
+        const obj = {};
+        if(obj[name] = e.toDate()){
+            this.props.onUserInput(obj);
+        }
+    }
+
+    render() {
+        const inputProps = {
+            name: 'appt_time'
+        };
+
+        return (
+            <div>
+                <h2>Make a new Appointment</h2>
+                <form onSubmit={(event) => this.handleSubmit(event)}>
+                    
+                    <input name='title' placeholder='Appointment Title' 
+                    value={this.props.title} onChange={(event) => this.handleChange(event)}/>
+        
+                    <Datetime input={false} open={true} inputProps={inputProps}
+                        value={this.props.appt_time}
+                        onChange={(event) => this.setApptTime(event)}/>
+
+                    <input type='submit' value='Make Appointment' className='submit-button'/>
+                </form>
+            </div>
+        )
+    }
+}
---
+++ app/javascript/packs/appointments.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppointmentForm from './appointment_form'
+import { AppointmentsList } from './appointments_list'
+import update from 'immutability-helper'
+
+export default class Appointments extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+          appointments: this.props.appointments,
+          title: 'Team standup meeting',
+          appt_time: 'Tomorrow at 9am'
+      }
+  }
+
+  handleUserInput(obj) {
+    this.setState(obj)
+  }
+
+  handleFormSubmit(){
+    const appointment = {title: this.state.title, appt_time: this.state.appt_time};
+    $.post('/appointments', 
+          {appointment: appointment})
+          .done((data) => {
+            this.addNewAppointment(data);
+          });
+  }
+
+  addNewAppointment(appointment){
+    const appointments = update(this.state.appointments, {$push: [appointment]});
+    this.setState({appointments: appointments.sort(function(a,b){
+      return new Date(a.appt_time) - new Date(b.appt_time);
+    })});
+  }
+
+  render() {
+     return (
+      <div>
+        <AppointmentForm 
+                         title={this.state.title} 
+                         appt_time={this.state.appt_time}
+                         onUserInput={(obj) => this.handleUserInput(obj)}
+                         onFormSubmit={() => this.handleFormSubmit()}/>
+        <AppointmentList appointments={this.state.appointments} />
+      </div>
+    )
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+ const node = document.getElementById('appointments_data')
+ const data = JSON.parse(node.getAttribute('data'))
+
+ReactDOM.render(
+ <Appointments appointments={data} />,
+ document.body.appendChild(document.createElement('div')),
+ )
+})
